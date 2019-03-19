7f690bcc1d2280314e2b18f3afd4bc28726a1396
VgasuIST <vgasu.ist@mail.ru>
2019-03-19 20:04:01 +0400

    Добавлены вспомогательные методы для денормализации координат обнаруженных объектов на изображении

--- src/recognition/DetectedObject.py
+++ src/recognition/DetectedObject.py
@@ -26,9 +26,34 @@ class DetectedObject:
         self.x2 = x2
         self.y2 = y2
 
+    def denormalized_points(self, image):
+        """
+            Возвращает денормализованные координаты ограничительной рамки на изображении
+            :param image:
+            :return:
+        """
+
+        image_w = image.shape[1]
+        image_h = image.shape[0]
+        p1 = int(self.x1*image_w), int(self.y1*image_h)
+        p2 = int(self.x2*image_w), int(self.y2*image_h)
+        return p1, p2
+
+    def denormalized_center(self, image):
+        """
+            Возвращает денормализованные координаты центра ограничительной рамки на изображении
+            :param image: изображение
+            :return: координаты (x,y)
+        """
+
+        image_w = image.shape[1]
+        image_h = image.shape[0]
+        return int((self.x1+self.x2)*0.5*image_w), int((self.y1+self.y2)*0.5*image_h)
+
     def __str__(self):
         """
             Вспомогательный метод для вывода объекта в консоль
+            :return: строковое представление объекта
         """
         return '{0}({1:.2f} {2:.2f} -> {3:.2f} {4:.2f})'\
             .format(self.name, self.x1, self.y1, self.x2, self.y2)
@@ -36,5 +61,6 @@ class DetectedObject:
     def __repr__(self):
         """
            Вспомогательный метод для вывода объекта в консоль
+           :return: строковое представление объекта
         """
         return self.__str__()
